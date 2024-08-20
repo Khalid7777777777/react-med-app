5955549a9f7c0dabcea0cd8036b21702136ad74e
Khalid7777777777 <celfie82@gmail.com>
2024-08-20 18:54:50 -0400

    schreenshots added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:Mjc5NzUtY2VsZmll@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTE5NjMtY2VsZmll@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- server/index.js
+++ server/index.js
@@ -5,10 +5,24 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8181;
 
-
 // Middleware
 app.use(express.json());
-app.use(cors());
+
+// CORS configuration
+const allowedOrigins = [
+  'https://celfie82-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', 
+  'https://your-other-allowed-origin.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 // Connect to MongoDB
 connectToMongo();
@@ -21,11 +35,12 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
-  // Start the server
+// Start the server
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
