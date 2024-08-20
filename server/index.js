b33f025457209b357c704ecda8118b5213cfb175
Khalid7777777777 <celfie82@gmail.com>
2024-08-20 07:30:15 -0400

    deployed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8181;
 
+
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -20,12 +21,11 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Start the server
+  // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+console.log(`Server is running on port http://localhost:${PORT}`);
 });
